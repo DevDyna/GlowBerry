8e9c12aea62d71b94753d1b0f2679457ce69245b
DevDyna <andrearisorti2003@gmail.com>
2024-05-02 21:34:01 +0200

    Update system.js

--- server_scripts/base/system.js
+++ server_scripts/base/system.js
@@ -5,3 +5,22 @@ ServerEvents.tags("item", (event) => {
   event.add("glowberry:any", global.disabledItem);
   event.add("glowberry:clear_nbt", global.hiddenItems);
 });
+
+ServerEvents.recipes(event => {
+  //Dynamic Tag Remover//
+  event.remove({
+      output: [
+          '#glowberry:output',
+          '#glowberry:any',
+          '#glowberry:disabled',
+      ]
+  })
+
+  event.remove({
+      input: [
+          '#glowberry:input',
+          '#glowberry:any',
+          '#glowberry:disabled',
+      ]
+  })
+})
