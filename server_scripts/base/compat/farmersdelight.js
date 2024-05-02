1f6d8099801d5ce0ada8676d8f5b450c014f25cd
DevDyna <andrearisorti2003@gmail.com>
2024-05-02 20:43:07 +0200

    Delete farmersdelight.js

--- server_scripts/base/compat/farmersdelight.js
+++
@@ -1,5 +0,0 @@
-if (global.mods.farmersdelight)
-  ServerEvents.tags("block", (event) => {
-    //event.removeAll("farmersdelight:compost_activators")
-    //event.add("farmersdelight:compost_activators", <block-id>);
-  });
