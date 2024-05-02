85d3a544b059e6e404904351e7ee9ae5ec2d639d
DevDyna <andrearisorti2003@gmail.com>
2024-05-02 20:43:55 +0200

    Create CreateConversion.js

---
+++ startup_scripts/base/compat/create/CreateConversion.js
@@ -0,0 +1,25 @@
+let $MysteriousItemConversionCategory, $ConversionRecipe;
+
+if (global.mods.create) {
+  /**
+   *
+   * @param {item} input
+   * @param {item[]} output
+   */
+  global.startup.myst = function myst(input, output) {
+    StartupEvents.postInit((event) => {
+      $MysteriousItemConversionCategory = Java.loadClass(
+        "com.simibubi.create.compat.jei.category.MysteriousItemConversionCategory"
+      );
+      $ConversionRecipe = Java.loadClass(
+        "com.simibubi.create.compat.jei.ConversionRecipe"
+      );
+      if (!Array.isArray(output)) output = [output];
+      output.forEach((element) => {
+        $MysteriousItemConversionCategory.RECIPES.add(
+          $ConversionRecipe.create(input, element)
+        );
+      });
+    });
+  };
+}
