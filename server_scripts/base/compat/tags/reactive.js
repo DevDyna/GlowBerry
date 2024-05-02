4ac6084440f8007ad7b4aaad4b98a9788ec348fc
DevDyna <andrearisorti2003@gmail.com>
2024-05-02 20:43:22 +0200

    Create reactive.js

---
+++ server_scripts/base/compat/tags/reactive.js
@@ -0,0 +1,19 @@
+if (global.mod.reactive)
+  ServerEvents.tags("item", (event) => {
+    //event.removeAll("reactive:mind_sources")
+    //event.add("reactive:body_sources", <item-id>);
+  });
+
+/*
+#SOURCES NAME#
+reactive:body_sources
+reactive:caustic_sources
+reactive:curse_sources
+reactive:high_potency
+reactive:light_sources
+reactive:mind_sources
+reactive:soul_sources
+reactive:verdant_sources
+reactive:vital_sources
+reactive:warp_sources
+*/
