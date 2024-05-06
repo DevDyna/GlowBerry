c222a4aaae4ea06db10f95114d08a6cb8d0f472d
DevDyna <andrearisorti2003@gmail.com>
2024-05-06 22:25:12 +0200

    Create create.js

---
+++ server_scripts/base/compat/schemas/create.js
@@ -0,0 +1,35 @@
+if (global.startup.mods.create) {
+  ServerEvents.recipes((event) => {
+    /**
+     *
+     * @param {block[]} block block to click
+     * @param {item[]} item item to click
+     * @param {block[]} result block to place
+     */
+    global.server.recipes.compat.create.ItemApplicator =
+      function ItemApplicator(block, item, result) {
+        block.forEach((a) => {
+          item.forEach((b) => {
+            result.forEach((c) => {
+              event.custom({
+                type: "create:item_application",
+                ingredients: [
+                  {
+                    item: a,
+                  },
+                  {
+                    item: b,
+                  },
+                ],
+                results: [
+                  {
+                    item: c,
+                  },
+                ],
+              });
+            });
+          });
+        });
+      };
+  });
+}
