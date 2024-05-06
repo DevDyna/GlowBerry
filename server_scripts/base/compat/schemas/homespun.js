5c182e98fa8771f1ca7193d6591d629107b85898
DevDyna <andrearisorti2003@gmail.com>
2024-05-06 22:25:10 +0200

    Create homespun.js

---
+++ server_scripts/base/compat/schemas/homespun.js
@@ -0,0 +1,47 @@
+if (global.startup.mods.homespun) {
+  ServerEvents.recipes((event) => {
+    global.server.recipes.compat.homespun.crushing = function crushing(
+      input,
+      output,
+      count,
+      fluid,
+      amount
+    ) {
+      event.custom({
+        type: "homespun:tub_crushing",
+        input_item: {
+          item: input,
+        },
+        output_item: {
+          count: count,
+          item: output,
+        },
+        fluid_stack: {
+          fluid: fluid,
+          amount: amount,
+        },
+      });
+    };
+
+    global.server.recipes.compat.homespun.evaporating = function evaporating(
+      fluid,
+      amount,
+      time,
+      output,
+      count
+    ) {
+      event.custom({
+        type: "homespun:evaporating",
+        output_item: {
+          item: output,
+          count: count,
+        },
+        fluid_stack: {
+          fluid: fluid,
+          amount: amount,
+        },
+        time: time,
+      });
+    };
+  });
+}
