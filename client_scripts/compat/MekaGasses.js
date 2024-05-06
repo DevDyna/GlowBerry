9083098a2f381e691f674335c786f72ed9c62708
DevDyna <andrearisorti2003@gmail.com>
2024-05-07 00:04:04 +0200

    Delete MekaGasses.js

--- client_scripts/compat/MekaGasses.js
+++
@@ -1,5 +0,0 @@
-ClientEvents.lang('en_us',event => {
-    global.client.GasToFluid.forEach(element => {
-        event.add('gas.kubejs.'+element,id.charAt(0).toUpperCase() + id.slice(1)+" (gas)")
-    });
-})
