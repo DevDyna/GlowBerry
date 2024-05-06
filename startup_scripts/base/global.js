6a6b8843d416188ce5c2d769c88bff695c16567b
DevDyna <andrearisorti2003@gmail.com>
2024-05-07 00:03:38 +0200

    Update global.js

--- startup_scripts/base/global.js
+++ startup_scripts/base/global.js
@@ -1,16 +1,35 @@
-//priority 1
-global.startup.mods = { 
-  farmersdelight: Platform.isLoaded('farmersdelight'),
-  create : Platform.isLoaded('create'),
-  tombstone : Platform.isLoaded('tombstone'),
-  ten3 : Platform.isLoaded('ten3'),
-  mekanism: Platform.isLoaded('mekanism'),
-  twilightforest: Platform.isLoaded('twilightforest'),
-  homespun: Platform.isLoaded('homespun'),
-  ae2: Platform.isLoaded('ae2'),
-  pnc: Platform.isLoaded('pnc'),
-  reactive: Platform.isLoaded('reactive'),
-  bluepower: Platform.isLoaded('bluepower'),
+//priority 10
+global.startup = {
+  mods: {
+    farmersdelight: Platform.isLoaded("farmersdelight"),
+    create: Platform.isLoaded("create"),
+    tombstone: Platform.isLoaded("tombstone"),
+    ten3: Platform.isLoaded("ten3"),
+    mekanism: Platform.isLoaded("mekanism"),
+    twilightforest: Platform.isLoaded("twilightforest"),
+    homespun: Platform.isLoaded("homespun"),
+    ae2: Platform.isLoaded("ae2"),
+    pnc: Platform.isLoaded("pnc"),
+    reactive: Platform.isLoaded("reactive"),
+    bluepower: Platform.isLoaded("bluepower"),
+  },
 };
 
-global.client.tip(global.disabledItem,'Item Disabled')
+global.server = {
+  datagen: {
+    pnc: { keep: Boolean },
+  },
+  recipes: {
+    processing: {},
+    shaped : {},
+    shapeless : {},
+    stonecutter : {},
+    compat: {},
+  },
+};
+
+global.client = { DefaultTooltip: Boolean };
+
+global.any = {
+  enableExamples : Boolean
+}
