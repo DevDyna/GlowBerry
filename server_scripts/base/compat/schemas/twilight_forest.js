972f85a244aa32fdb95e44138f0fb1284d3f4e87
DevDyna <andrearisorti2003@gmail.com>
2024-05-08 14:31:08 +0200

    Update twilight_forest.js

--- server_scripts/base/compat/schemas/twilight_forest.js
+++ server_scripts/base/compat/schemas/twilight_forest.js
@@ -1,29 +1,34 @@
 if (global.startup.mods.twilightforest) {
-  ServerEvents.recipes((event) => {
-    /**
-     *
-     * @param {item} input
-     * @param {string[]} pattern like shaped recipes ['AAA','AAA','AAA']
-     * @param {item} output
-     * @param {int} output_count
-     * @param {int} cost experience cost
-     */
-    global.server.recipes.compat.twilightforest.uncrafting =
-      function uncrafting(input, pattern, output, output_count, cost) {
-        event.custom({
-          type: "twilightforest:uncrafting",
-          cost: cost,
-          count: output_count,
-          ingredient: {
-            item: input,
+  /**
+   *
+   * @param {item} input
+   * @param {string[]} pattern like shaped recipes ['AAA','AAA','AAA']
+   * @param {item} output
+   * @param {int} output_count
+   * @param {int} cost experience cost
+   */
+  global.server.recipes.compat.twilightforest.uncrafting = (
+    input,
+    pattern,
+    output,
+    output_count,
+    cost
+  ) => {
+    ServerEvents.recipes((event) => {
+      event.custom({
+        type: "twilightforest:uncrafting",
+        cost: cost,
+        count: output_count,
+        ingredient: {
+          item: input,
+        },
+        key: {
+          A: {
+            item: output,
           },
-          key: {
-            A: {
-              item: output,
-            },
-          },
-          pattern: pattern,
-        });
-      };
-  });
+        },
+        pattern: pattern,
+      });
+    });
+  };
 }
