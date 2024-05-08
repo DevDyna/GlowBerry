f5c26f224602060dd702e8d02fca0721667a3f35
DevDyna <andrearisorti2003@gmail.com>
2024-05-08 14:30:23 +0200

    Update create.js

--- server_scripts/base/compat/schemas/create.js
+++ server_scripts/base/compat/schemas/create.js
@@ -1,35 +1,42 @@
 if (global.startup.mods.create) {
-  ServerEvents.recipes((event) => {
-    /**
-     *
-     * @param {block[]} block block to click
-     * @param {item[]} item item to click
-     * @param {block[]} result block to place
-     */
-    global.server.recipes.compat.create.ItemApplicator =
-      function ItemApplicator(block, item, result) {
-        block.forEach((a) => {
-          item.forEach((b) => {
-            result.forEach((c) => {
-              event.custom({
-                type: "create:item_application",
-                ingredients: [
-                  {
-                    item: a,
-                  },
-                  {
-                    item: b,
-                  },
-                ],
-                results: [
-                  {
-                    item: c,
-                  },
-                ],
-              });
+  /**
+   *
+   * @param {block|block[]} block block to click
+   * @param {item|item[]} item item to click
+   * @param {block|block[]} result block to place
+   */
+  global.server.recipes.compat.create.ItemApplicator = (
+    block,
+    item,
+    result
+  ) => {
+    ServerEvents.recipes((event) => {
+      if (!Array.isArray(block)) block = [block];
+      if (!Array.isArray(item)) item = [item];
+      if (!Array.isArray(result)) result = [result];
+
+      block.forEach((a) => {
+        item.forEach((b) => {
+          result.forEach((c) => {
+            event.custom({
+              type: "create:item_application",
+              ingredients: [
+                {
+                  item: a,
+                },
+                {
+                  item: b,
+                },
+              ],
+              results: [
+                {
+                  item: c,
+                },
+              ],
             });
           });
         });
-      };
-  });
+      });
+    });
+  };
 }
